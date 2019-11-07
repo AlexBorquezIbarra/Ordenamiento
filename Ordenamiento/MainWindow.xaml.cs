98a6335c941c98343823b135beb9ea8e67ad0c55
alumno <alumno@E122>
2019-11-07 07:51:58 -0700

    Memaso

--- Ordenamiento/MainWindow.xaml.cs
+++ Ordenamiento/MainWindow.xaml.cs
@@ -22,6 +22,8 @@ namespace Ordenamiento
     public partial class MainWindow : Window
     {
         ObservableCollection<int> miLista = new ObservableCollection<int>();
+        ObservableCollection<Alumno> alumnos = new ObservableCollection<Alumno>();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -34,19 +36,20 @@ namespace Ordenamiento
             miLista.Add(13);
             miLista.Add(404);
 
-            alumnos.Add(new Alumno("aaaa", 9.1f, 2));
-            alumnos.Add(new Alumno("Jgse", 9.2f, 23));
-            alumnos.Add(new Alumno("Joge", 9.4f, 1));
-            alumnos.Add(new Alumno("Jgse", 9.6f, 0));
-            alumnos.Add(new Alumno("Jose", 9.3f, 3));
-            alumnos.Add(new Alumno("Joge", 8.1f, 3));
-            alumnos.Add(new Alumno("Jdse", 5.1f, 7));
-            alumnos.Add(new Alumno("Jofe", 4.1f, 8));
+            alumnos.Add(new Alumno("pepe", 9.1f, 2));
+            alumnos.Add(new Alumno("Jose", 9.2f, 23));
+            alumnos.Add(new Alumno("sara", 9.4f, 1));
+            alumnos.Add(new Alumno("oscar", 9.6f, 0));
+            alumnos.Add(new Alumno("juan", 9.3f, 3));
+            alumnos.Add(new Alumno("Julio", 8.1f, 3));
+            alumnos.Add(new Alumno("ramon", 5.1f, 7));
+            alumnos.Add(new Alumno("pita", 4.1f, 8));
 
             lstNumeros.ItemsSource = alumnos;
             //lstNumeros.ItemsSource = miLista;
         }
 
+        //Shell Promedio
         private void BtnOrdenar_Click(object sender, RoutedEventArgs e)
         {
             /*var temp = miLista[0];
@@ -71,6 +74,7 @@ namespace Ordenamiento
             }
         }
 
+        //Bubble Promedio
         private void BtnOrdenarBubble_Click(object sender, RoutedEventArgs e)
         {
             bool intercambio = false;
@@ -79,11 +83,53 @@ namespace Ordenamiento
                 intercambio = false;
                 for (int i = 0; i < miLista.Count - 1; i++)
                 {
-                    if (miLista[i] > miLista[i + 1])
+                    if (alumnos[i].promedio > alumnos[i + 1].promedio)
                     {
-                        int temp = miLista[i];
-                        miLista[i] = miLista[i + 1];
-                        miLista[i + 1] = temp;
+                        var temp = alumnos[i];
+                        alumnos[i] = alumnos[i + 1];
+                        alumnos[i + 1] = temp;
+                        intercambio = true;
+                    }
+                }
+            } while (intercambio);
+        }
+
+        //Shell Faltas
+        private void BtnOrdenarShellFaltas_Click(object sender, RoutedEventArgs e)
+        {
+            int gap, i, j;
+            gap = alumnos.Count / 2;
+            //ahhh
+            while (gap > 0)
+            {
+                for (i = 0; i < alumnos.Count; i++)
+                {
+                    if (gap + i < alumnos.Count && alumnos[i].faltas > alumnos[gap + i].faltas)
+                    {
+                        var temp = alumnos[i];
+                        alumnos[i] = alumnos[gap + i];
+                        alumnos[gap + i] = temp;
+                    }
+                }
+                gap--;
+            }
+        }
+
+        //Bubble Faltas
+
+        private void BtnOrdenarBubbleFaltas_Click(object sender, RoutedEventArgs e)
+        {
+            bool intercambio = false;
+            do
+            {
+                intercambio = false;
+                for (int i = 0; i < miLista.Count - 1; i++)
+                {
+                    if (alumnos[i].faltas > alumnos[i + 1].faltas)
+                    {
+                        var temp = alumnos[i];
+                        alumnos[i] = alumnos[i + 1];
+                        alumnos[i + 1] = temp;
                         intercambio = true;
                     }
                 }
